342c9d5539f3b1e2d3de79b5ea7e2c0a8c736abe
usuario <usuario@10.22.143.50>
2016-09-30 10:52:44 +0100

    Kata 2, version 0

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -2,27 +2,27 @@ package kata2;
 
 import java.util.HashMap;
 
-public class Histogram {
-    private final int[] vector;
+public class Histogram<T> {
+   private final T[] vector;
 
-    public Histogram(int[] vector) {
+    public Histogram(T[] vector) {
         this.vector = vector;
     }
 
-    public int[] getVector() {
+    public T[] getVector() {
         return vector;
     }
-    
-    public HashMap<Integer,Integer> getHistogram(){
-        HashMap<Integer,Integer> histogram =new HashMap<>();
-         for (int key:vector){
-            if(histogram.containsKey(key)){
-                histogram.put(key,histogram.get(key)+1);
-            }else{
-                histogram.put(key,1);
+   
+    public HashMap<T,Integer> getHistogram(){
+        HashMap<T,Integer> histogram=new HashMap<>();
+        for (T key : vector) {
+            if(!histogram.containsKey(key)){
+                histogram.put(key,0);
             }
+            histogram.put(key, histogram.get(key)+1);
         }
-         return histogram;
+        return histogram;
     }
-}
+   
+   }
 
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,10 +5,12 @@ import java.util.HashMap;
 public class Kata2 {
     
     public static void main(String[] args) {
-        int[] vector={2,2,11,4,2,6,11,2,3,5,11,5};
+        Integer [] vector={2,2,11,4,2,6,11,2,3,5,11,5};
+      //  Double[] vector={2.1,2.1,11.3,4.2,2.1,4.2,11.3,5.5,3.7,5.3,11.3,5.5};
+      //  String[] vector={"Sarah","Cristina","Jaime","Sarah","Jorge","Cristina","Judith","C.Judith"};
         Histogram histo=new Histogram(vector);
-        HashMap<Integer,Integer> histogram = histo.getHistogram();
-        for(int key : histogram.keySet()){
+        HashMap<Object,Integer> histogram = histo.getHistogram();
+        for(Object key : histogram.keySet()){
             System.out.println(key + "-->" + histogram.get(key));
         }
     }
